6593dd29455c4fcbe1b07355c28299b3d5b11493
EhssanBx0 <ehssanbaghban@outlook.com>
2025-08-31 23:14:04 +0100

    add event listener to operator btns to enable user input

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const multiply = (x,y) => x*y;
 const divide = (x,y) => x/y;
 
 const operate = (operation, x, y) => operation(x,y);
+
+const selectOperator = (event) => {
+}
+
+const operatorBtns = document.querySelector(".operatorBtns");
+operatorBtns.addEventListener("click", selectOperator);
